fc3533e68d70900ca4ee1541cf430fc8f288759d
vsamaru <vsamaru@gmail.com>
2020-04-16 18:12:44 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@
 module.exports = function(config) {
 
   // A useful way to reference the context we are runing eleventy in
-  let env = process.env.ELEVENTY_ENV;
+  let env = 'prod'
 
   // Layout aliases can make templates more portable
   config.addLayoutAlias('default', 'layouts/base.njk');
